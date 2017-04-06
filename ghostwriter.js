f9c8027f073b6094a3aae9baf4fc422db339a5b5
Unknown <mshimada2@mail.csuchico.edu>
2017-04-06 13:29:36 -0700

    edited the code so you would get random numbers from the arrays

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,9 +1,20 @@
 function writeTweet()
 {
-  
+var randIndex;
+var sentence;
+var intro = ["Chaluppa Batman", "Forever Unclean", "Ruxin", "Lil mama has a swag like mine"];
+var body = ["Taco", "EBDB", "1,2,3 and to the 4", "Last name ever first name greatest", "Baby wont you come my wayyy"];
+var end =  ["1738 aye", "Hey girl would you like to be my sunshine",];
+randIndex = randomUpTo(intro.length-1);
+sentence = intro[randIndex];
+randIndex = randomUpTo(body.length-1);
+sentence = sentence +" "+ body[randIndex];
+randIndex = randomUpTo(end.length-1);
+sentence = sentence +" "+ end[randIndex];
+document.getElementById("tweet").innerHTML = sentence;
 }
 
-//Generates a random whole number between 0 and max (inclusive)
+
 function randomUpTo(max)
 {
   return Math.floor(Math.random() * (max+1));
